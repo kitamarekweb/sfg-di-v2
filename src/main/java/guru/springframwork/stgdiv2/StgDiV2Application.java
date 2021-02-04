5ff135ebcc6aae41c4b5c09d2c8a0b61ef9de732
mk <kitamarekweb@gmail.com>
2021-02-04 14:23:13 +0100

    32. The Spring Context - Spring create new instance of the Class and put it into the ApplicationContext

--- src/main/java/guru/springframwork/stgdiv2/StgDiV2Application.java
+++ src/main/java/guru/springframwork/stgdiv2/StgDiV2Application.java
@@ -1,13 +1,23 @@
 package guru.springframwork.stgdiv2;
 
+import guru.springframwork.stgdiv2.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StgDiV2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StgDiV2Application.class, args);
+		ApplicationContext ctx = SpringApplication.run(StgDiV2Application.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		myController.sayHello();
+
+		System.out.println(greeting);
 	}
 
 }
---
+++ src/main/java/guru/springframwork/stgdiv2/controllers/MyController.java
@@ -0,0 +1,12 @@
+package guru.springframwork.stgdiv2.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello() {
+        System.out.println("Hello World!!");
+        return "Hi Folks!";
+    }
+}
