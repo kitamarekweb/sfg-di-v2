99f17566b19ccc51c3609839feaf3df973b8cef9
mk <kitamarekweb@gmail.com>
2021-02-04 14:05:04 +0100

    Initial Project Add

---
+++ src/main/java/guru/springframwork/stgdiv2/StgDiV2Application.java
@@ -0,0 +1,13 @@
+package guru.springframwork.stgdiv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StgDiV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StgDiV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframwork/stgdiv2/StgDiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframwork.stgdiv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StgDiV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
