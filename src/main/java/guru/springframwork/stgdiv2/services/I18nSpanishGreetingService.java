65ae237b5b2a5bb4a21a1fc72954b49f02b59ce0
mk <kitamarekweb@gmail.com>
2021-02-05 13:32:05 +0100

    39. Default Profile

--- src/main/java/guru/springframwork/stgdiv2/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframwork/stgdiv2/services/I18nSpanishGreetingService.java
@@ -3,12 +3,12 @@ package guru.springframwork.stgdiv2.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "Profile ES: Hola in SPANISH";
+        return "Profile ES (also default): Hola in SPANISH";
     }
 }
